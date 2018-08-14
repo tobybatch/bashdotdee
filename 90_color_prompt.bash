b171d1606ec286263255a0acc669ee20732b4d25
Toby Batch <tobias@neontribe.co.uk>
2018-08-14 13:48:20 +0100

    Added .env support for hostname colour

--- 90_color_prompt.bash
+++ 90_color_prompt.bash
@@ -1,5 +1,19 @@
+if [ -f $HOME/.env ]; then
+    source $HOME/.env
+fi
+
+if [ -z "$HOSTNAME_COLOUR" ]; then
+    HOSTNAME_COLOUR='38;5;33m'
+fi
+
 if [ "$TERM" == "screen" ]; then
   PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]\$ '
 fi
 
-export PROMPT_COMMAND='__git_ps1 "\[$(tput bold)\]\[\033[38;5;10m\]\u\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;33m\]\h\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;87m\]\W\[$(tput sgr0)\]" "$(__drush_ps1 "[%s]") \\\$ "'
+PS1_GIT="__git_ps1 "
+PS1_USER='\[$(tput bold)\]\[\033[38;5;10m\]\u\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\]'
+PS1_HOST='\[$(tput bold)\]\[$(tput sgr0)\]\[\033['$HOSTNAME_COLOUR'\]\h\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\]'
+PS1_DIR='\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;87m\]\W\[$(tput sgr0)\]'
+PS1_DRUSH='$(__drush_ps1 "[%s]") \\\$ '
+
+export PROMPT_COMMAND="$PS1_GIT \"$PS1_USER@$PS1_HOST:$PS1_DIR\" \"$PS1_DRUSH\""
