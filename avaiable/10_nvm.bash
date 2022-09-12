3dc6b30e815a61a4a80e17d9802f8f4cd1fa857f
Toby Batch <tobias@neontribe.co.uk>
2022-09-12 16:39:34 +0100

    Update 10_nvm.bash

--- avaiable/10_nvm.bash
+++ avaiable/10_nvm.bash
@@ -1,5 +1,13 @@
+if [ -z "$NVM_V" ]; then
+  export NVM_V=14.15.3
+fi
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-# nvm use 10.20.1
-nvm use 14.15.3
+
+if [ ! -d "${HOME}/.nvm/versions/node/v${NVM_V}" ]; then
+  nvm install ${NVM_V}
+fi
+
+nvm use $NVM_V
