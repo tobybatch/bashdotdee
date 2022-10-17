e3bc1a2ee3135e89a642fabf2cbdbae33596661d
Tobias Batch <tobias@neontribe.co.uk>
2022-10-17 14:59:51 +0100

    feat: added NCVO

---
+++ avaiable/88_NCVO.bash
@@ -0,0 +1,1 @@
+PATH=$PATH:/home/tobias/usr/ncvo/back-controller/bin
--- avaiable/91_magicmonty.bash
+++ avaiable/91_magicmonty.bash
@@ -1,4 +1,13 @@
 # https://github.com/magicmonty/bash-git-prompt/blob/master/README.md
+# git_prompt_make_custom_theme Single_line_Ubuntu
+# sed -i 's/${White}${Time12a}${ResetColor} //g' .git-prompt-colors.sh
+
+GIT_PROMPT_THEME=Single_line_Ubuntu
+
+if [ -f "$HOME/.git-prompt-colors.sh" ]; then
+    GIT_PROMPT_THEME=Custom
+    source $HOME/.git-prompt-colors.sh
+fi
 
 if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
