244c5225d8e7dbe7dc7404c300643f3cbd924f89
Tobias Batch <tobias@neontribe.co.uk>
2023-07-21 11:16:45 +0100

    feat: allow multipole enables in a single command

--- bash.d.sh
+++ bash.d.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-BASHDDIR=$(dirname $0)
+BASHDDIR=$(dirname "$0")
 
-USAGE="`basename $0` [-e <config>|-d config|-l]"
+# shellcheck disable=SC2086
+USAGE="$(basename $0) [-e <config>|-d config|-l]"
 
 function usage {
-    echo $USAGE
+    echo "$USAGE"
     cat <<EOF
 Enable, disable or list available bash config. Run without any parameters it
 will list currently enabled configs.
@@ -19,39 +20,48 @@ EOF
 }
 
 function enable_config {
-  SOURCE=$BASHDDIR/avaiable/$1
-  TARGET=$BASHDDIR/enabled/$1
+  # first arg is the -e so renove it
+  shift
+  exit_state=0
+  for src in "$@"; do
+      SOURCE=$BASHDDIR/available/$src
+      TARGET=$BASHDDIR/enabled/$src
 
-  if [ ! -e "$SOURCE" ]; then
-    echo "No such config ($1)"
-    exit 1
-  fi
+      if [ ! -e "$SOURCE" ]; then
+        echo "No such config ($1)"
+        exit_state=1
+      fi
 
-  if [ -L "$TARGET" ]; then
-    echo "Config already enabled ($1)"
-    exit 1
-  fi
+      if [ -L "$TARGET" ]; then
+        echo "Config already enabled ($1)"
+        exit_state=1
+      fi
 
-  ln -s $(realpath $SOURCE) $TARGET
+      # shellcheck disable=SC2046
+      ln -s $(realpath "$SOURCE") "$TARGET"
+      echo "Enabled $src"
+  done
+  exit "$exit_state"
 }
 
 function disable_config {
   TARGET=$BASHDDIR/enabled/$1
-  rm -f $TARGET
+  rm -f "$TARGET"
 }
 
 function list_configs {
-  ls -1 $BASHDDIR/avaiable
+  ls -1 "$BASHDDIR/available"
 }
 
 while getopts "e:d:lh" options; do
     case $options in
-        e) enable_config $OPTARG; exit;;
-        d) disable_config $OPTARG; exit;;
+        e) enable_config "$@"; exit;;
+        d) disable_config "$@"; exit;;
         l) list_configs; exit;;
-        h) usage; exit;;
+        h) ;;
+        *) usage; exit;;
     esac
 done
 
 # If we got this far without exiting it's a list currently enabled
-ls -1 $BASHDDIR/enabled
+ls -1 "$BASHDDIR/enabled"
