f053534ca4554f7bd37e6f6cb33b34c7c550a6a7
Toby Batch <tobias@neontribe.co.uk>
2024-11-20 11:16:17 +0000

    feat@ added mac specific stuff

--- available/10_nvm.bash
+++ available/10_nvm.bash
@@ -2,7 +2,11 @@ if [ -z "$NVM_V" ]; then
   export NVM_V=18.14.1
 fi
 
-export NVM_DIR="$HOME/.nvm"
+if [[ $OSTYPE == 'darwin'* ]]; then
+  export NVM_DIR=/opt/homebrew/Cellar/nvm/0.40.1
+else
+  export NVM_DIR="$HOME/.nvm"
+fi
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
@@ -10,6 +14,6 @@ if [ ! -d "${HOME}/.nvm/versions/node/v${NVM_V}" ]; then
   nvm install ${NVM_V}
 fi
 
-export PATH=$PATH:/home/tobias/.nvm/versions/node/v${NVM_V}/bin/workbox
+export PATH=$PATH:$HOME/.nvm/versions/node/v${NVM_V}/bin/workbox
 
 nvm use $NVM_V > /dev/null
--- available/19_fix_macos.bash
+++ available/19_fix_macos.bash
@@ -1,12 +1,33 @@
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo source $HOME/.bashrc >> .bash_profile
+# for x in git bash-git-prompt jq nmap npm bat completion; do brew install $x; done
+
 # Enable bash completion if present
 [ -f /opt/homebrew/etc/bash_completion ] && . /opt/homebrew/etc/bash_completion
 
+export PATH=$PATH:/opt/homebrew/bin
+
 # Some standard aliases
-alias cat='batcat'
+alias cat='bat'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias grep='grep --color=auto'
 alias l='ls -CF'
 alias la='ls -A'
 alias ll='ls -alF'
-alias ls='ls --color=auto'
+alias ls='ls --color'
+
+if [ ! -e $HOME/Library/KeyBindings ]; then
+  mkdir -p $HOME/Library/KeyBindings
+  echo '{
+/* Remap Home / End keys to be correct */
+"\UF729" = "moveToBeginningOfLine:"; /* Home */
+"\UF72B" = "moveToEndOfLine:"; /* End */
+"$\UF729" = "moveToBeginningOfLineAndModifySelection:"; /* Shift + Home */
+"$\UF72B" = "moveToEndOfLineAndModifySelection:"; /* Shift + End */
+"^\UF729" = "moveToBeginningOfDocument:"; /* Ctrl + Home */
+"^\UF72B" = "moveToEndOfDocument:"; /* Ctrl + End */
+"$^\UF729" = "moveToBeginningOfDocumentAndModifySelection:"; /* Shift + Ctrl + Home */
+"$^\UF72B" = "moveToEndOfDocumentAndModifySelection:"; /* Shift + Ctrl + End */
+}' > $HOME/Library/KeyBindings/DefaultKeyBinding.dict
+fi
