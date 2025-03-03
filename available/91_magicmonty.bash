bd354850250beef8d94a45d19a23d174c6019e0e
Toby Batch <tobias@neontribe.co.uk>
2025-03-03 11:00:03 +0000

    fix: add macos support to a bunch of scripts

--- available/91_magicmonty.bash
+++ available/91_magicmonty.bash
@@ -9,10 +9,12 @@ if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     source $HOME/.bash-git-prompt/gitprompt.sh
 fi
 
-if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
-  __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
-  GIT_PROMPT_ONLY_IN_REPO=1
-  source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+if [[ $OSTYPE == 'darwin'* ]]; then
+  if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+    __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+    GIT_PROMPT_ONLY_IN_REPO=1
+    source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+  fi
 fi
 
 if [ -f "$HOME/.git-prompt-colors.sh" ]; then
