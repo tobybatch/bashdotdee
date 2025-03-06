6412937e61f0b76d0ec3274d2c6eb645de5ef979
Toby Batch <tobias@neontribe.co.uk>
2025-03-06 21:45:46 +0000

    fix: new insall script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TARGET=/tmp
+
+if [ ! -d "$TARGET/.bash.d" ]; then
+  git clone https://github.com/tobybatch/bashdotdee.git $TARGET/.bash.d
+fi
+
+echo '' >> $TARGET/.bashrc
+echo 'PATH=\$PATH:\$HOME/.bash.d/bin' >> $TARGET/.bashrc
+echo 'for x in ~/.bash.d/enabled/*.bash' >> $TARGET/.bashrc
+echo 'do' >> $TARGET/.bashrc
+echo '    . "\$x"' >> $TARGET/.bashrc
+echo 'done' >> $TARGET/.bashrc
+
+ln -sf $TARGET/.bash.d/available/01_path.bash $TARGET/.bash.d/enabled/01_path.bash
