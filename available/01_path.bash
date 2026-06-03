f41e85a5c1e1ac9be6946c8116c04a9d8c4e517b
Toby Batch <tobias@neontribe.co.uk>
2026-06-03 08:45:25 +0100

    fix: moved homebrew to start of the PATH

--- available/01_path.bash
+++ available/01_path.bash
@@ -2,4 +2,4 @@ export PATH=$PATH:$HOME/bin
 export PATH=$PATH:$HOME/.bash.d/bin
 export PATH=$PATH:$HOME/.local/bin
 [ -d "$HOME/.screenlayout" ] &&export PATH=$PATH:$HOME/.screenlayout
-[ -d "/opt/homebrew/bin" ] && export PATH=$PATH:/opt/homebrew/bin
+[ -d "/opt/homebrew/bin" ] && export PATH=/opt/homebrew/bin:$PATH
--- available/02_bash_completion.bash
+++ available/02_bash_completion.bash
@@ -6,8 +6,8 @@ if [ ! -e "$HOME/.local/bash_completion.d/bash.d.bash" ]; then
 fi
 
 # a little klunky but this does not fail on various OS's
-[ -f /opt/homebrew/etc/bash_completion ] && . /opt/homebrew/etc/bash_completion
-[ -f /opt/homebrew/share/bash-completion/bash_completion ] && ./opt/homebrew/share/bash-completion/bash_completion
+[ -f /opt/homebrew/etc/bash_completion ] && source /opt/homebrew/etc/bash_completion
+[ -f /opt/homebrew/share/bash-completion/bash_completion ] && source /opt/homebrew/share/bash-completion/bash_completion
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
 [ -d "$HOME"/.local/bash_completion.d ] && for x in "$HOME"/.local/bash_completion.d/*.bash; do source "$x"; done
 [ -d "$HOME"/.bash.d/bash_completion.d ] && for x in "$HOME"/.bash.d/bash_completion.d/*.bash; do source "$x"; done
